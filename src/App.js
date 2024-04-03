d2fe9eb377818b6c53868148ceed9e09c58297e3
msingh <msingh@cogitate.us>
2024-04-03 13:43:24 +0530

    initial commit 2

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Converter App
+      </header>
+      <div className="features">
+        <button className="feature">CSV 2 JSON</button>
+        <button className="feature">JSON 2 CSV</button>
+        <button className="feature">XLSX 2 JSON</button>
+        <button className="feature">JSON 2 XLXS</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
